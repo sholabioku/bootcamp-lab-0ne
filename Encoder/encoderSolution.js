53389a07b515ffe48c11094fde7da88157f9bb08
sholabioku <bilushonline@gmail.com>
2019-04-16 15:13:11 +0100

    First commit

---
+++ Encoder/encoderSolution.js
@@ -0,0 +1,23 @@
+// The Encoder function takes two arrays as its parameters, raw and code_words.
+// This function replaces the words in `raw` with the words in `code_words` such that the first occurrence of
+// each word in `raw` is assigned the first unassigned word in `code_words`
+
+function encoderSolution(raw, code_words) {
+    const dict = {};
+    let count =0;
+
+    new Set(raw).forEach((key) => {
+      dict[key] = code_words[count];
+      count++;
+    });
+
+    const encoded = [];
+    raw.forEach((key) => {
+      encoded.push(dict[key]);
+    });
+
+    return encoded;
+
+}
+
+module.exports = encoderSolution;
---
+++ Interpret/interpretSolution.js
@@ -0,0 +1,43 @@
+// This function takes three parameters, the first is a number and the second and third are arrays.
+// The second parameter is an array of operations which cold contain one or more of "+", "-", and "*"
+// The third array is an arry of numbers which are the second participants in the operations
+// If you encounter an unknown command in the second array, return -1.
+
+function compute(result, commands, numbers, commandType){
+
+    if( commands[commandType] === '*') {
+
+        return result * numbers[commandType];
+
+    } else if( commands[commandType] === '+') {
+
+        return result + numbers[commandType];
+    } else if( commands[commandType] === '-') {
+
+        return result - numbers[commandType];
+    } else {
+
+        return undefined;
+    };
+
+};
+
+function interpret(firstNum, commands, numbers){
+
+    let result = firstNum;
+
+    for (let i=0; i< commands.length; i++) {
+
+      result = compute(result, commands, numbers, i);
+
+      if(result === undefined){
+           return -1;
+        }
+
+    };
+
+    return result;
+
+};
+
+module.exports = interpret;
